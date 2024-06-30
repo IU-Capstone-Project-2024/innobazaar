0abc4a771abac3d8ea5d90f3c935421ee94a1d8f
Kamil116 <kamil.mirgasimoff@yandex.ru>
2024-06-30 07:20:22 +0300

    Connected backend and frontend, enhanced authorization

--- frontend/src/components/Authorization/SignIn.js
+++ frontend/src/components/Authorization/SignIn.js
@@ -2,40 +2,83 @@ import React from 'react';
 import './styles/Authorization.css';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import axios from 'axios';
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+    baseURL: "http://127.0.0.1:8000"
+});
+
+function HandleSubmit(e) {
+    e.preventDefault();
+    console.log(e)
+    let email = document.getElementById('email-input-login').value
+    let password = document.getElementById('password-input-login').value
+
+    client.post(
+        "/api/login",
+        {
+            email: email,
+            password: password
+        }
+    ).then(() => {
+        window.location.replace('/')
+    })
+        .catch((error) => {
+            const login_error_text = document.getElementById("login-error")
+            login_error_text.style.display = 'block'
+            login_error_text.textContent = error.response.data[0]
+            login_error_text.style.fontSize = '18px'
+            login_error_text.style.margin = '0 0 10px 0'
+            login_error_text.style.padding = '2px 0 2px 0'
+            login_error_text.style.color = 'red'
+            login_error_text.style.textAlign = 'center'
+            login_error_text.style.border = '2px solid red'
+        })
+}
 
 const SignIn = () => (
     <div>
-        <Header />
+        <Header/>
         <div id="middle">
             <div id="login-window">
-                <div class="sign-buttons">
-                    <div class="sign-in-in" id='sign-in'>
-                        <button class="wide_button" id="sign-in-button-login" onclick="{signInClicked()}">
+                <div className="sign-buttons">
+                    <div className="sign-in-in" id='sign-in'>
+                        <button className="wide_button" id="sign-in-button-login">
                             Sign In
                         </button>
                     </div>
-                    <div class="sign-up-in" id='sign-up'>
+                    <div className="sign-up-in" id='sign-up'>
                         <Link to='/signup'>
-                            <button class="wide_button" id="sign-up-button-login" onclick="{signUpClicked()}">Sign Up</button>
+                            <button className="wide_button" id="sign-up-button-login">Sign Up</button>
                         </Link>
                     </div>
                 </div>
-                <div class="main-content">
-                    <label for="email-input-login">
-                        Email address
-                        <input type="text" class="input_box" id="email-input-login" />
-                    </label>
-                    <label for="password-input-login">
-                        Password
-                        <input type="text" class="input_box" id="password-input-login" />
-                    </label>
-                    <button class="wide_button" id="bottom_sign_in">SIGN IN</button>
+                <div className="main-content">
+                    <div>
+                        <p id="login-error" style={{display: "none"}}></p>
+                    </div>
+                    <form id="login-form" onSubmit={HandleSubmit}>
+                        <label htmlFor="email-input-login">
+                            Email address
+                            <input type="email" className="input_box" id="email-input-login" required/>
+                        </label>
+                        <label htmlFor="password-input-login">
+                            Password
+                            <input type="password" className="input_box" id="password-input-login" required/>
+                        </label>
+                        <button className="wide_button" id="bottom_sign_in">SIGN IN</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <Footer />
+        <Footer/>
     </div>
 );
+
 
 export default SignIn;
